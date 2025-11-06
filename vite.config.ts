b3f5837b99b6d91e15d8ecb36da2352b9ced70d8
Benjamin Saint-augustin <ben.saint.augustin@gmail.com>
2025-11-06 11:12:50 +0100

    fix: vite erreur windows

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	server: {
+		host: '0.0.0.0', // Permet d'accéder depuis le conteneur
+		watch: {
+			usePolling: true, // Force la détection via polling (Docker + Windows/macOS)
+			interval: 300, // Vérifie les changements toutes les 300 ms
+		},
+	},
 })
