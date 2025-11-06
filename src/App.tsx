d81ac35c6787bae70fb318747d0ba00ad80bb1f2
Lorlavie <laure.lavie@gmail.com>
2025-11-06 16:11:08 +0100

    feat(carousel): add carousel slide container and text (#25)
    Co-authored-by: LaureLavie <laure.laviegmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Footer from './components/Footer.tsx'
+import Carousel from './components/Carousel.tsx'
 import Header from './components/Header.tsx'
 
 function App() {
 	return (
 		<>
+			<Carousel />
 			<Header />
 			<Footer />
 		</>
