1d35ef059648226ffeecf5fd94fe7c38490f5f8c
BenSTAU <ben.saint.augustin@gmail.com>
2025-11-06 16:19:34 +0100

    Feat/header nav (#26)
    * feat: footer version finale et header première version
    
    * fix: correction background
    
    * fix: correction import
    
    * fix: correction taille et centrage du footer
    
    ---------
    
    Co-authored-by: yannick biot <yannickpro64@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import Footer from './components/Footer.tsx'
-import Carousel from './components/Carousel.tsx'
 import Header from './components/Header.tsx'
 
 function App() {
 	return (
 		<>
-			<Carousel />
 			<Header />
 			<Footer />
 		</>
