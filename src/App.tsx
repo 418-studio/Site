049241c2cb4930d9909028c5523241c4a62539ee
Benjamin Saint-augustin <ben.saint.augustin@gmail.com>
2025-11-06 11:34:36 +0100

    style: ajout du style de base et des fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
-	const [count, setCount] = useState(0)
-
-	return (
-		<>
-			<h1>Vite + React 1</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-		</>
-	)
+	return <></>
 }
 
 export default App
