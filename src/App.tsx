a8725ba3bb7b1618631f6e915519f158523c68e8
Benjamin Saint-augustin <ben.saint.augustin@gmail.com>
2025-11-07 11:25:21 +0100

    fix: correction import app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import Footer from './components/Footer.tsx'
-import Carousel from './pages/Carousel.tsx'
 import SectionOurTeam from './pages/SectionOurTeam.tsx'
 import Header from './components/Header.tsx'
 import Carousel from './pages/Carousel.tsx'
