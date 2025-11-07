9e3a774a9d4912b1861670bdd640a2d5bc8e1174
BenSTAU <ben.saint.augustin@gmail.com>
2025-11-07 10:20:06 +0100

    Feat/hero (#33)
    * fix: correction header et nav css
    
    * fix: taille header
    
    * feat: hero v1 mobile
    
    ---------
    
    Co-authored-by: yannick biot <yannickpro64@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Footer from './components/Footer.tsx'
 import SectionOurTeam from './pages/SectionOurTeam.tsx'
 import Header from './components/Header.tsx'
+import Hero from './pages/Hero.tsx'
 
 function App() {
 	return (
 		<>
 			<Header />
+			<Hero />
 			<SectionOurTeam />
 			<Footer />
 		</>
