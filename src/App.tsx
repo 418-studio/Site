b60f009a4ae48b20d484dd18a09a94ce7b66613d
yannou64 <yannickpro64@gmail.com>
2025-11-07 11:50:33 +0100

    feat(sectionContact): ajout de la version mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Footer from './components/Footer.tsx'
 import SectionOurTeam from './pages/SectionOurTeam.tsx'
 import Header from './components/Header.tsx'
+import SectionContact from './pages/SectionContact.tsx'
 
 function App() {
 	return (
 		<>
 			<Header />
 			<SectionOurTeam />
+			<SectionContact />
 			<Footer />
 		</>
 	)
