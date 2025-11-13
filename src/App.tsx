5d28177c7d229d85c02d83e4c1657d21c98a1a12
Benjamin Saint-augustin <ben.saint.augustin@gmail.com>
2025-11-13 11:31:15 +0100

    feat: modif du caroussel en version mobile etc et ajout des fichiers supprimés

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import Footer from './components/Footer.tsx'
 import SectionOurTeam from './pages/SectionOurTeam.tsx'
 import Header from './components/Header.tsx'
+import SectionContact from './pages/SectionContact.tsx'
 import Carousel from './pages/Carousel.tsx'
 import Hero from './pages/Hero.tsx'
+import SectionTitreOurTeam from './pages/SectionTitreOurTeam.tsx'
+import BentoGrid from './pages/BentoGrid.tsx'
 
 function App() {
 	return (
@@ -10,7 +13,10 @@ function App() {
 			<Header />
 			<Hero />
 			<Carousel />
+			<SectionTitreOurTeam />
 			<SectionOurTeam />
+			<BentoGrid />
+			<SectionContact />
 			<Footer />
 		</>
 	)
